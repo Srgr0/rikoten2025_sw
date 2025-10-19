63dd990405e4a451c8b5538b171a4a9a0318e794
Srgr0 <66754887+Srgr0@users.noreply.github.com>
2025-10-19 10:46:51 +0900

    良さそう

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 os.makedirs(PROCESSED_DIR, exist_ok=True)
 
 ADMIN_PASS = st.secrets.ADMIN_PASS  # ← 企画側パスコード
-EXPIRE_SECONDS = 15  # 1時間で削除
+EXPIRE_SECONDS = 3600  # 1時間で削除
 
 
 # -----------------------------
